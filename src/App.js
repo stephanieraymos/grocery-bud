ca0f167d6ef0d9361968e5168f51f81a213509d3
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 14:00:08 -0800

    Baseplate

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
+
+function App() {
+  return <h2>Grocery bud</h2>;
+}
+
+export default App;
