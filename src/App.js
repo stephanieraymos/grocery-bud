af54d4eb67c7eba22c0f80cacd1d772f5f4281ad
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-24 14:09:39 -0800

    State + form + handleSubmit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,25 @@ import List from "./List";
 import Alert from "./Alert";
 
 function App() {
-  return <h2>Grocery bud</h2>;
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editId, setEditId] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Testing");
+  };
+
+  return (
+    <section className="section-center">
+      <form className="grocery-form" onSubmit={handleSubmit}></form>
+      <div className="grocery-container">
+        <List />
+        <button className="clear-btn">Clear items</button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
