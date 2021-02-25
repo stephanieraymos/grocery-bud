a329eda287a379931ba3a28fcf8ef8a530c952bc
Stephanie Raymos <58127737+stephanieraymos@users.noreply.github.com>
2021-02-25 09:51:44 -0800

    Setting up alert

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
   const [list, setList] = useState([]);
   const [isEditing, setIsEditing] = useState(false);
   const [editId, setEditId] = useState(null);
-  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+  const [alert, setAlert] = useState({ show: true, msg: "message", type: "danger" });
   const handleSubmit = (e) => {
     e.preventDefault();
     if (!name) {
@@ -15,17 +15,17 @@ function App() {
     } else if (name && isEditing) {
       // deal with edit if something is in value and user is editing
     } else {
-      // Show alert
+      // Show alert and add item to list only if name is true and not editing
       const newItem = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItem]);
-      setName("");
+      setName(""); //Reseting input box to empty string
     }
   };
 
   return (
     <section className="section-center">
       <form className="grocery-form" onSubmit={handleSubmit}>
-        {alert.show && <Alert />}
+        {alert.show && <Alert {...alert}/>}
         <h3>Grocery Bud</h3>
         <div className="form-control">
           <input
